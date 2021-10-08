9086fc4cde286064960f7151ea878825409fb1da
Daniel Maestre <dmaestre@cifpfbmoll.eu>
2021-10-08 21:07:28 +0200

    Classe dia 8

--- DWEC/Exercicis/EC02/src/js/script.js
+++ DWEC/Exercicis/EC02/src/js/script.js
@@ -18,7 +18,7 @@ function comptadorDeA() {
     let num = 0;
     for (let i = 0; i < paraula.length; i++) {
         if (paraula.charAt(i) == "a" || paraula.charAt(i) == "A") {
-            num++;
+            num++; // Cada vegada que troba una a minúscula o majúscula, es compta 1
         }
     }
     document.getElementById("resultado").innerHTML = "Hi ha " + num + " lletres 'a' en aquest string";
@@ -42,7 +42,7 @@ function firstlastA() {
         if (paraula.charAt(i) == "a") {
             document.getElementById("resultado").innerHTML = 
             "La posició de la primera lletra (a minúscula) és " + paraula.indexOf("a",0) + " i la darrera és " + paraula.lastIndexOf("a",paraula.length);
-        } else if (paraula.charAt(i) == "A") {
+        } else if (paraula.charAt(i) == "A") { // D'aquesta manera el faig case-sensitive
             document.getElementById("resultado").innerHTML = 
             "La posició de la lletra (A majúscula) és " + paraula.indexOf("A",0) + " i la darrera és " + paraula.lastIndexOf("A",paraula.length);
         }
@@ -51,5 +51,52 @@ function firstlastA() {
 
 function extractor() {
     let paraula = document.getElementById("uno").value;
+    document.getElementById("resultado").innerHTML = paraula.replaceAll("la", "");
+}
+
+function substitutor() {
+    let paraula = document.getElementById("uno").value;
+    document.getElementById("resultado").innerHTML = paraula.replaceAll("la", "*");
+}
+
+function capicua() {
+    let paraula = document.getElementById("uno").value;
+    document.getElementById("resultado").innerHTML = paraula.charAt(0)+paraula+paraula.charAt(0);
+}
+
+function foraEspai() {
+    let paraula = document.getElementById("uno").value;
+    document.getElementById("resultado").innerHTML = paraula.replaceAll(" ", "");
+}
+
+function guionizator() {
+    // He transformat els espais per guions pas per pas (m'ha sortit d'aquesta manera)
+    let paraula = document.getElementById("uno").value;
+    paraula = paraula.replaceAll("  ", "-");
+    paraula = paraula.replaceAll(" ", "-");
+    for (let i = 0; i < paraula.length; i++) {
+        paraula = paraula.replaceAll("--", "-");
+    }
+    document.getElementById("resultado").innerHTML = paraula;
+}
+
+function acronimator() {
+    let paraula = document.getElementById("uno").value;
     document.getElementById("resultado").innerHTML = "Codi en construcció";
+}
+
+function email() {
+    let paraula = document.getElementById("uno").value;
+    document.getElementById("resultado").innerHTML = "Codi en construcció";
+}
+
+function signeNum() {
+    let num = document.getElementById("uno").value;
+    if (num < 0) {
+        document.getElementById("resultado").innerHTML = num + " es de signe negatiu.";
+    } else if (num > 0) {
+        document.getElementById("resultado").innerHTML = num + " es de signe positiu.";
+    } else {
+        document.getElementById("resultado").innerHTML = num + " es 0, ni + ni -.";
+    }
 }
